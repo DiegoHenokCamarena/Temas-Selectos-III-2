7d1aacbc81d1bfc84c678a757e1ab928ac21329e
diegoHCR <diego.camarena@ingenieria.unam.edu>
2022-03-29 00:17:39 -0600

    ps4 controllers

--- Practica5/Assets/Codes/ps4Controller.cs
+++ Practica5/Assets/Codes/ps4Controller.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ps4Controller : MonoBehaviour
 {
+    GameObject mov = null;
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < Gamepad.all.Count; i++)
+        {
+            Debug.Log(Gamepad.all[i].name);
+        }
         
+        mov  = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Gamepad.all.Count > 0)
+        {
+            if (Gamepad.all[0].leftStick.up.isPressed)
+            {
+                mov.transform.position += Vector3.left * Time.deltaTime * 500f;
+            }
+                
+            if (Gamepad.all[0].leftStick.down.isPressed)
+            {
+                mov.transform.position += Vector3.right * Time.deltaTime * 500f;
+            }
+
+            if (Gamepad.all[0].rightStick.right.isPressed)
+            {
+                mov.transform.position += Vector3.right * Time.deltaTime * 500f;
+            }
+        }
+            
     }
 }
